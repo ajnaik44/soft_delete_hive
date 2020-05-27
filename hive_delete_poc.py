7bd8034d3e53dd0c53c13cb22be8a11a0c1645a8
Ajay <ajay.naik44@gmail.com>
2020-05-27 15:06:33 +0530

    Update hive_delete_poc.py

--- hive_delete_poc.py
+++ hive_delete_poc.py
@@ -7,25 +7,15 @@ user_tracker = spark.read.option("header","true").csv(r"D:\clairvoyant\deletepoc
 userDS = spark.read.option("header","true").csv(r"D:\clairvoyant\deletepoc\user.csv")
 
 user_tracker_record = user_tracker.collect()
-
-
-
-metadataDS = spark.read.option("header","true").csv(r"D:\clairvoyant\deletepoc\usecase_metadata.csv")
-
+userDS = spark.read.option("header","true").csv(r"E:\ClairvoyantWork\DeleteUseCase\user.csv")
+user_metadata_ds = spark.read.option("header","true").csv(r"E:\ClairvoyantWork\DeleteUseCase\usecase_metadata.csv")
 for row in user_tracker_record:
-    metadata = metadataDS.filter(metadataDS.usecase_name==row['usecase_name'] ).filter(metadataDS.table_name==row['table_name'] )
-    identifier_key_value = row['identifier_key']
-    latestRecord =    userDS.where(col(identifier_key_value)==row['identifier_value'])
-    print(metadata.select("column_name").collect()[0]['column_name'])
-    latestRecord.show()
-    latestRecordtoUpdate = latestRecord.select("*",when(latestRecord.is_deleted == "FALSE",metadata.select("updated_value").collect()[0][0]).alias(metadata.select("column_name").collect()[0][0]))
-    latestRecordtoUpdate.show()
-    #latesRecord.at[0,metadata.select("column_name").collect()[0][0]]= metadata.select("updated_value").collect()[0][0]
-    #latesRecord.identifier_key_value = metadata.updated_value
-    #userDS.where(identifier_key_value == row['identifier_value']).show
-    
-   
-    
-    
-    
-    
+    metadata = user_metadata_ds.filter(user_metadata_ds.usecase_name==row['usecase_name']).filter(user_metadata_ds.table_name==row['table_name'])
+    identifier_key = row['identifier_key']
+    print(identifier_key)
+    latestRecord = userDS.where(col(identifier_key)==row['identifier_value'])
+    updated_col_detail = metadata.select("column_name","updated_value").collect();
+    col_name = updated_col_detail[0]['column_name']
+    col_value = updated_col_detail[0]['updated_value']
+    s = list(set(latestRecord.columns) - {col_name})
+    latestRecord.select(*s,when(col(col_name).isNotNull(),col_value).alias(col_name)).show()
